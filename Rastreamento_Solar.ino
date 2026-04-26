0dd43bc311ebd813e36e4a5e9f05f82c6e69f610
joaocc0sta <ccosta.joao13@gmail.com>
2026-04-26 11:51:14 -0300

    1.0.0 Implementação da conexão do ESP32 via WiFi

--- Rastreamento_Solar.ino
+++ Rastreamento_Solar.ino
@@ -1,9 +1,37 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h> //Biblioteca conexão Wifi
 
+const char* ssid = "CJ"; //Rede
+const char* password = "12345678"; // Senha da Rede
+
+
+
+void setup(){
+    Serial.begin(115200);
+    pinMode(2, OUTPUT);
+    
+    Serial.print("Connecting to "); 
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password); //Comando para conectar na rede
+
+    while (WiFi.status() != WL_CONNECTED){
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("Wifi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP()); //Endereço da conexão
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop(){
+  conecta_rede ();
 }
+
+void conecta_rede (){
+  digitalWrite(2, HIGH);
+  delay(2000);
+  digitalWrite(2, LOW);
+  delay(2000);
+}
+
