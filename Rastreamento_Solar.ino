24bda3a72753c1075240e2bd8e85b28ee0f9fbab
joaocc0sta <ccosta.joao13@gmail.com>
2026-05-02 14:46:00 -0300

    Problema com conexão Wi-Fi com rede local (modem), apenas o acesso remoto ao celular está conectando. Inclusão da biblioteca ThingSpeak e pequenas alterações na estrutura do código.

--- Rastreamento_Solar.ino
+++ Rastreamento_Solar.ino
@@ -1,9 +1,10 @@
 #include <WiFi.h> //Biblioteca conexão Wifi
+#include <ThingSpeak.h> //Biblioteca conexão ThingSpeak
 
 const char* ssid = "CJ"; //Rede
 const char* password = "12345678"; // Senha da Rede
-
-
+const long channel = 3366372; //Canal utilizado
+const char *WRITE_API = "W7ZQLMMVG5GXUQF0"; //Senha de acesso API
 
 void setup(){
     Serial.begin(115200);
@@ -22,10 +23,19 @@ void setup(){
     Serial.println("Wifi connected.");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP()); //Endereço da conexão
+
+    ThingSpeak.begin(client); //Incializa o ThingSpeak
 }
 
 void loop(){
   conecta_rede ();
+  int estado_led;
+  estado_led = digitalRead(2);
+  Serial.print("LED = ");
+  Serial.println(estado_led);
+  delay(1000);
+ 
+  conecta_nuvem ();
 }
 
 void conecta_rede (){
@@ -33,5 +43,9 @@ void conecta_rede (){
   delay(2000);
   digitalWrite(2, LOW);
   delay(2000);
+}
+
+void conecta_nuvem(){
+
 }
 
